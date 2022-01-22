9bf1737c371d331d7b615fb140cd05041ba9c3ea
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-01-22 10:44:28 +0100

    Create Mod.cs

---
+++ Fronter.NET/Models/Mod.cs
@@ -0,0 +1,17 @@
+﻿using commonItems;
+using Fronter.NET.ViewModels;
+
+namespace Fronter.Models;
+
+public class Mod : ViewModelBase {
+	public Mod(string modPath) {
+		var parser = new Parser();
+		parser.RegisterKeyword("name", reader => Name = reader.GetString());
+		parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreItem);
+
+		parser.ParseFile(modPath);
+		FileName = CommonFunctions.TrimPath(modPath);
+	}
+	public string Name { get; private set; } = string.Empty;
+	public string FileName { get; private set; }
+}
