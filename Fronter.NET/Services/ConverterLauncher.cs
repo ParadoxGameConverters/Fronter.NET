0769d6f4c31990f408b0818d4596876a1fddc30a
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-01-30 01:21:21 +0100

    Create ConverterLauncher.cs

---
+++ Fronter.NET/Services/ConverterLauncher.cs
@@ -0,0 +1,29 @@
+﻿using commonItems;
+using Fronter.Models.Configuration;
+using System.IO;
+
+namespace Fronter.Services;
+
+internal class ConverterLauncher {
+	public ConverterLauncher() {
+		var converterFolder = Path.Combine(configuration.ConverterFolder);
+		var backendExePath = Path.Combine(configuration.BackendExePath);
+		var backendExePathRelativeToFrontend = Path.Combine(converterFolder, backendExePath);
+
+		var extension = CommonFunctions.GetExtension(backendExePathRelativeToFrontend);
+		if (extension == "jar") {
+			backendExePathRelativeToFrontend = $"java.exe -jar {backendExePathRelativeToFrontend}";
+		} else if (string.IsNullOrEmpty(extension)) {
+			backendExePathRelativeToFrontend += ".exe";
+		}
+
+		if (string.IsNullOrEmpty(backendExePath)) {
+			Logger.Error("Converter location has not been set!");
+		}
+		if (!File.Exists(backendExePathRelativeToFrontend)) {
+			Logger.Error("Could not find converter executable!");
+		}
+	}
+
+	private Configuration configuration;
+}
