f10d21a648e45ebf1b3b25d00745f1b704ef1375
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-03-20 18:26:20 +0100

    RequiredFoldersAndFilesAreLoaded test

--- Fronter.NET.Tests/Models/ConfigurationTests.cs
+++ Fronter.NET.Tests/Models/ConfigurationTests.cs
@@ -18,4 +18,44 @@ public class ConfigurationTests {
 		Assert.Equal("https://forum.paradoxplaza.com/forum/threads/imperator-to-ck3-release-thread.1415172", config.ConverterReleaseForumThread);
 		Assert.Equal("https://paradoxgameconverters.com/commit_ids/ImperatorToCK3.txt", config.PagesCommitIdUrl);
 	}
+
+	[Fact]
+	public void RequiredFoldersAndFilesAreLoaded() {
+		var config = new Configuration();
+		Assert.Collection(config.RequiredFolders,
+			kvPair => {
+				(string key, RequiredFolder folder) = kvPair;
+				Assert.Equal("ImperatorDirectory",key);
+				Assert.Equal("IMPFOLDER", folder.DisplayName);
+			},
+			kvPair => {
+				(string key, RequiredFolder folder) = kvPair;
+				Assert.Equal("ImperatorDocDirectory",key);
+				Assert.Equal("IMPDOCTIP", folder.Tooltip);
+			},
+			kvPair => {
+				(string key, RequiredFolder folder) = kvPair;
+				Assert.Equal("CK3directory",key);
+				Assert.True(folder.Mandatory);
+			},
+			kvPair => {
+				(string key, RequiredFolder folder) = kvPair;
+				Assert.Equal("targetGameModPath",key);
+				Assert.Equal("Paradox Interactive\\Crusader Kings III\\mod", folder.SearchPath);
+			}
+		);
+		
+		Assert.Collection(config.RequiredFiles,
+			kvPair => {
+				(string key, RequiredFile file) = kvPair;
+				Assert.Equal("SaveGame", file.Name);
+				Assert.Equal("IMPSAVE", file.DisplayName);
+				Assert.Equal("IMPSAVETIP", file.Tooltip);
+				Assert.True(file.Mandatory);
+				Assert.True(file.Outputtable);
+				Assert.Equal("windowsUsersFolder", file.SearchPathType);
+				Assert.Equal("Paradox Interactive\\Imperator\\save games", file.SearchPath );
+				Assert.Equal("*.rome", file.AllowedExtension);
+			});
+	}
 }
