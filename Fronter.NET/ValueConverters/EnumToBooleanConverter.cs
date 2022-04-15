e1377b888613216bcd9e1b42d992c54f601a7fcc
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-04-15 22:53:19 +0200

    EnumToBooleanConverter cleanup

--- Fronter.NET/ValueConverters/EnumToBooleanConverter.cs
+++ Fronter.NET/ValueConverters/EnumToBooleanConverter.cs
@@ -1,21 +1,19 @@
 ﻿using Avalonia;
 using Avalonia.Data.Converters;
-using commonItems;
 using System;
 using System.Globalization;
 
 namespace Fronter.ValueConverters;
 
-public class EnumToBooleanConverter : IValueConverter
-{
-	public static EnumToBooleanConverter Instance = new EnumToBooleanConverter();
+public class EnumToBooleanConverter : IValueConverter {
+	public static readonly EnumToBooleanConverter Instance = new();
 
-	public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+	public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
 		return value?.Equals(parameter) ?? AvaloniaProperty.UnsetValue;
 	}
 
-	public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
-		if (value != null) {
+	public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+		if (value is not null) {
 			return (bool)value ? parameter : AvaloniaProperty.UnsetValue;
 		}
 		return AvaloniaProperty.UnsetValue;
