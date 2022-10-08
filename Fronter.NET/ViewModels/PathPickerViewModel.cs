45bf7c63499aa779b61e57d69c8e1373df7d3094
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-10-08 22:46:37 +0200

    Use reactive commands for path dialogs

--- Fronter.NET/ViewModels/PathPickerViewModel.cs
+++ Fronter.NET/ViewModels/PathPickerViewModel.cs
@@ -1,10 +1,15 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using Avalonia.Platform.Storage.FileIO;
 using commonItems;
 using Fronter.Extensions;
 using Fronter.Models.Configuration;
 using Fronter.Views;
+using ReactiveUI;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
 
 namespace Fronter.ViewModels;
 
@@ -15,10 +20,17 @@ public class PathPickerViewModel : ViewModelBase {
 	public PathPickerViewModel(Configuration config) {
 		RequiredFolders = new ObservableCollection<RequiredFolder>(config.RequiredFolders);
 		RequiredFiles = new ObservableCollection<RequiredFile>(config.RequiredFiles);
+		
+		// Create reactive commands.
+		OpenFolderDialogCommand = ReactiveCommand.Create<RequiredFolder>(OpenFolderDialog);
+		OpenFileDialogCommand = ReactiveCommand.Create<RequiredFile>(OpenFileDialog);
 	}
 
 	public ObservableCollection<RequiredFolder> RequiredFolders { get; }
 	public ObservableCollection<RequiredFile> RequiredFiles { get; }
+	
+	public ReactiveCommand<RequiredFolder, Unit> OpenFolderDialogCommand { get; }
+	public ReactiveCommand<RequiredFile, Unit> OpenFileDialogCommand { get; }
 
 	public async void OpenFolderDialog(RequiredFolder folder) {
 		var dlg = new OpenFolderDialog {
@@ -32,25 +44,28 @@ public class PathPickerViewModel : ViewModelBase {
 		}
 	}
 	public async void OpenFileDialog(RequiredFile file) {
-		var dlg = new OpenFileDialog {
-			Title = TranslationSource.Instance[file.DisplayName]
+		var options = new FilePickerOpenOptions {
+			Title = TranslationSource.Instance[file.DisplayName],
+			AllowMultiple = false
 		};
+		
 		if (file.InitialDirectory is not null) {
-			dlg.Directory = file.InitialDirectory;
+			options.SuggestedStartLocation = new BclStorageFolder(file.InitialDirectory);
 		} else if (!string.IsNullOrEmpty(file.Value)) {
-			dlg.Directory = CommonFunctions.GetPath(file.Value);
+			options.SuggestedStartLocation = new BclStorageFolder(CommonFunctions.GetPath(file.Value));
 		}
 
-		var filter = new FileDialogFilter {Extensions = {file.AllowedExtension.TrimStart('*', '.')}};
-		if (dlg.Filters is null) {
-			dlg.Filters = new List<FileDialogFilter> {filter};
-		} else {
-			dlg.Filters.Add(filter);
-		}
+		var fileType = new FilePickerFileType(file.AllowedExtension.TrimStart('*', '.')) {
+			Patterns = new[] {file.AllowedExtension}
+		};
+		options.FileTypeFilter = new List<FilePickerFileType> {fileType};
 
-		var result = await dlg.ShowAsync(MainWindow.Instance);
-		if (result is not null) {
-			file.Value = result[0];
+		var window = MainWindow.Instance;
+		var result = await window.StorageProvider.OpenFilePickerAsync(options);
+		var selectedFile = result.FirstOrDefault(defaultValue: null);
+
+		if (selectedFile is not null) {
+			file.Value = selectedFile.Name;
 		}
 	}
 }
