aff078ebb13bdeb2594ef5beea539e2f3625939c
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-10-19 18:09:40 +0200

    Upgrade MessageBox.Avalonia

--- Fronter.NET/ViewModels/PathPickerViewModel.cs
+++ Fronter.NET/ViewModels/PathPickerViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Avalonia.Controls;
-using Avalonia.Platform.Storage;
+﻿using Avalonia.Platform.Storage;
 using Avalonia.Platform.Storage.FileIO;
 using commonItems;
 using Fronter.Extensions;
