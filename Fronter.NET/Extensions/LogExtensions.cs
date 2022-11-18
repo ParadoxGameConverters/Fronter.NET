90cb2702b4bc97345a2e81fc0aa0378a1deff6c0
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-11-18 23:20:37 +0100

    Disable warning about non-UTC timestamp, local one is fine

--- Fronter.NET/Extensions/LogExtensions.cs
+++ Fronter.NET/Extensions/LogExtensions.cs
@@ -7,7 +7,9 @@ namespace Fronter.Extensions;
 public static class LogExtensions {
 	public static void LogWithCustomTimestamp(this ILog log, DateTime timestamp, Level level, string message) {
 		var loggingEventDate = new LoggingEventData {
+#pragma warning disable CS0618
 			TimeStamp = timestamp, Level = level, Message = message
+#pragma warning restore CS0618
 		};
 		var loggingEvent = new LoggingEvent(loggingEventDate);
 		log.Logger.Log(loggingEvent);
