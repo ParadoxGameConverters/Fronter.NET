800a5f7f9590574107ecb29f98d22f9c8eda896b
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-12-03 22:56:02 +0100

    Get rid of `dynamic` from UpdateChecker

---
+++ Fronter.NET/Models/ConverterReleaseAsset.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Fronter.Models; 
+
+public class ConverterReleaseAsset {
+	[JsonPropertyName("name")] public string Name { get; }
+	[JsonPropertyName("name")] public string BrowserDownloadUrl { get; }
+}
---
+++ Fronter.NET/Models/ConverterReleaseInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Fronter.Models; 
+
+public class ConverterReleaseInfo {
+	[JsonPropertyName("body")] public string? Body { get; }
+	[JsonPropertyName("name")] public string? Name { get; }
+	[JsonPropertyName("assets")] public List<ConverterReleaseAsset>? Assets { get; } = new();
+}
