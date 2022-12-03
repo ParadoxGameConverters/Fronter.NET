0146863b2ea430784fc1afadfd7966481a600590
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-12-03 22:58:04 +0100

    Add setters

--- Fronter.NET/Models/ConverterReleaseInfo.cs
+++ Fronter.NET/Models/ConverterReleaseInfo.cs
@@ -4,7 +4,7 @@ using System.Text.Json.Serialization;
 namespace Fronter.Models; 
 
 public class ConverterReleaseInfo {
-	[JsonPropertyName("body")] public string? Body { get; }
-	[JsonPropertyName("name")] public string? Name { get; }
-	[JsonPropertyName("assets")] public List<ConverterReleaseAsset>? Assets { get; } = new();
+	[JsonPropertyName("body")] public string? Body { get; set; }
+	[JsonPropertyName("name")] public string? Name { get; set; }
+	[JsonPropertyName("assets")] public List<ConverterReleaseAsset>? Assets { get; set; } = new();
 }
