a5b618b344b762d28c2cbe392ddf09d89c8f812e
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-01-08 11:58:12 +0100

    Never filter out Notice log messages, change default log filter to Warn (#178)
    * Never filter out Notice log messages
    
    * Set default log filter to Warn

--- Fronter.NET/LogAppenders/LogGridAppender.cs
+++ Fronter.NET/LogAppenders/LogGridAppender.cs
@@ -19,13 +19,14 @@ public class LogGridAppender : MemoryAppender {
 	private ReadOnlyObservableCollection<LogLine> filteredLogLines;
 	public ReadOnlyObservableCollection<LogLine> FilteredLogLines => filteredLogLines;
 
-	public Level LogFilterLevel = Level.Info;
+	public Level LogFilterLevel = Level.Warn;
 
 	public DataGrid? LogGrid { get; set; }
 	
 	public LogGridAppender() {
+		// The idea of notice in the converters was to display the notice regardless of filtering level.
 		LogLines.ToObservableChangeSet()
-			.Filter(line => line.Level >= LogFilterLevel)
+			.Filter(line => line.Level == Level.Notice || line.Level >= LogFilterLevel)
 			.Bind(out filteredLogLines)
 			.Subscribe();
 	}
