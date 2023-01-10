731e26886b56b9173e1f404ddd236280b548b7ea
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-01-11 00:56:29 +0100

    Force Release config in build_frontend action  (#181)
    * make Release config first in Fronter.NET.sln
    
    * Force Release config in build_frontend action

--- .github/actions/build_frontend/entrypoint.sh
+++ .github/actions/build_frontend/entrypoint.sh
@@ -9,15 +9,15 @@ then
   cd "${FRONTER_DIR}/Fronter.NET"
   if [ "$RUNNER_OS" = "Windows" ]
   then
-    dotnet publish -p:PublishProfile=win-x64 --output:"${GITHUB_WORKSPACE}/${RELEASE_DIR}"
+    dotnet publish -c Release -p:PublishProfile=win-x64 --output:"${GITHUB_WORKSPACE}/${RELEASE_DIR}"
   
   elif [ "$RUNNER_OS" = "Linux" ]
   then
-    dotnet publish -p:PublishProfile=linux-x64 --output:"${GITHUB_WORKSPACE}/${RELEASE_DIR}"
+    dotnet publish -c Release -p:PublishProfile=linux-x64 --output:"${GITHUB_WORKSPACE}/${RELEASE_DIR}"
  
   elif [ "$RUNNER_OS" = "macOS" ]
   then
-    dotnet publish -p:PublishProfile=osx-x64 --output:"${GITHUB_WORKSPACE}/${RELEASE_DIR}"
+    dotnet publish -c Release -p:PublishProfile=osx-x64 --output:"${GITHUB_WORKSPACE}/${RELEASE_DIR}"
   
   fi
   cd "$GITHUB_WORKSPACE"
