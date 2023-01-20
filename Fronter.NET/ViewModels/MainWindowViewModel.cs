fff0ba2d200ec83692c3988bbf709ae8a5ec1f00
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-01-21 00:00:29 +0100

    Set MaxHeight for update message box (#191)

--- Fronter.NET/ViewModels/MainWindowViewModel.cs
+++ Fronter.NET/ViewModels/MainWindowViewModel.cs
@@ -220,7 +220,8 @@ public class MainWindowViewModel : ViewModelBase {
 				ButtonDefinitions = new[] {
 					new ButtonDefinition {Name = updateNow, IsDefault = true},
 					new ButtonDefinition {Name = maybeLater, IsCancel = true}
-				}
+				},
+				MaxHeight = 720
 			});
 		var result = await messageBoxWindow.ShowDialog(MainWindow.Instance);
 		if (result != updateNow) {
