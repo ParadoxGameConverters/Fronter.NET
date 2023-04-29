db6decfb97d9829fa3ddbc0040c2d78c008774b4
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-04-29 10:54:00 +0200

    Enable compiled bindings

---
+++ Fronter.NET/Models/FrontendTheme.cs
@@ -0,0 +1,8 @@
+﻿using commonItems.Collections;
+
+namespace Fronter.Models; 
+
+public sealed class FrontendTheme : IIdentifiable<string> {
+	public required string Id { get; init; }
+	public required string LocKey { get; init; }
+}
