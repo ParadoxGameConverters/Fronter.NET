f2b5f928349346acb6c4b68ce9d324154ac32b10
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-05-02 19:08:12 +0200

    DateSelector

---
+++ Fronter.NET/Models/Configuration/Options/DateSelector.cs
@@ -0,0 +1,45 @@
+﻿using commonItems;
+using System;
+
+namespace Fronter.Models.Configuration.Options; 
+
+public class DateSelector : Selector {
+	public DateSelector(BufferedReader reader) {
+		var parser = new Parser();
+		RegisterKeys(parser);
+		parser.ParseStream(reader);
+	}
+	private void RegisterKeys(Parser parser) {
+		parser.RegisterKeyword("editable", reader => Editable = reader.GetString() == "true");
+		parser.RegisterKeyword("value", reader => {
+			var valueStr = reader.GetString();
+			Value = string.IsNullOrWhiteSpace(valueStr) ? null : new Date(valueStr);
+		});
+		parser.RegisterKeyword("tooltip", reader => Tooltip = reader.GetString());
+		parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
+	}
+
+	public bool Editable { get; private set; } = true; // editable unless disabled
+	public DateTimeOffset? DateTimeOffsetValue { get; set; }
+
+	public Date? Value {
+		get {
+			if (DateTimeOffsetValue is null) {
+				return null;
+			}
+
+			var offsetValue = DateTimeOffsetValue.Value;
+			return new Date(offsetValue.Year, offsetValue.Month, offsetValue.Day);
+		}
+		set {
+			if (value is null) {
+				DateTimeOffsetValue = null;
+			} else {
+				var dt = new DateTime(value.Year, value.Month, value.Day);
+				DateTimeOffsetValue = new DateTimeOffset(dt);
+			}
+		}
+	}
+
+	public string? Tooltip { get; private set; }
+}
