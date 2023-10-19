463aebed1055e7d41da6a26870e7d153b74a2115
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-10-19 23:52:44 +0100

    LoggingConfiguratorTests

---
+++ Fronter.NET.Tests/Services/LoggingConfiguratorTests.cs
@@ -0,0 +1,28 @@
+﻿using log4net;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace Fronter.Tests.Services; 
+
+public class LoggingConfiguratorTests {
+	[Fact]
+	public void MessagesAreLoggedToLogTxtFile() {
+		LoggingConfigurator.ConfigureLogging(useConsole: false);
+		
+		var logger = LogManager.GetLogger(typeof(LoggingConfiguratorTests));
+		logger.Debug("Test debug message");
+		logger.Info("Test message");
+		logger.Warn("Test warning");
+		logger.Error("Test error");
+
+		using var fs = new FileStream("log.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+		using var sr = new StreamReader(fs, Encoding.Default);
+		
+		var logFileContent = sr.ReadToEnd();
+		Assert.Contains("Test debug message", logFileContent);
+		Assert.Contains("Test message", logFileContent);
+		Assert.Contains("Test warning", logFileContent);
+		Assert.Contains("Test error", logFileContent);
+	}
+}
