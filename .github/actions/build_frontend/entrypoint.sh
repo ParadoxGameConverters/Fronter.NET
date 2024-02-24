e6675407ccc8efe1d1ae67929cc7ebab22f2ed98
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2024-02-24 13:20:26 +0100

    Build updater with `onefile` option

--- .github/actions/build_frontend/entrypoint.sh
+++ .github/actions/build_frontend/entrypoint.sh
@@ -36,7 +36,7 @@ then
   pip3 install pip-tools
   python3 -m piptools compile -o requirements.txt pyproject.toml
   pip3 install -r requirements.txt 
-  python3 -m PyInstaller --icon=updater.ico updater.py
+  python3 -m PyInstaller --onefile --icon=updater.ico updater.py
   mkdir -p "${GITHUB_WORKSPACE}/${RELEASE_DIR}/Updater"
   mv dist/* "${GITHUB_WORKSPACE}/${RELEASE_DIR}/Updater/"
 
