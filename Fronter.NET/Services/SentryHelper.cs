60d44347e2189c0751d1737577de61381badab37
IhateTrains <IhateTrains@users.noreply.github.com>
2024-06-10 17:01:39 +0100

    Only init Sentry SDK when user wants to upload save (#552)
    * Only init Sentry SDK when user wants to upload save
    
    * satisfy CodeFactor

--- Fronter.NET/Services/SentryHelper.cs
+++ Fronter.NET/Services/SentryHelper.cs
@@ -1,15 +1,71 @@
-﻿using Fronter.LogAppenders;
+﻿using commonItems;
+using Fronter.LogAppenders;
 using Fronter.Models;
+using Fronter.Models.Configuration;
 using log4net;
 using log4net.Core;
 using Sentry;
 using System;
+using System.IO;
 using System.Linq;
 
 namespace Fronter.Services;
 
-public static class SentryHelper {
-	public static void SendMessageToSentry(int processExitCode) {
+public class SentryHelper { 
+	public SentryHelper(Config config) {
+		this.config = config;
+		InitSentry();
+	}
+	
+	private readonly Config config;
+	
+	private void InitSentry() {
+		string? release = null;
+		// Try to get version from converter's version.txt
+		var versionFilePath = Path.Combine(config.ConverterFolder, "configurables/version.txt");
+		if (File.Exists(versionFilePath)) {
+			var version = new ConverterVersion();
+			version.LoadVersion(versionFilePath);
+			if (!string.IsNullOrWhiteSpace(version.Version) && !string.IsNullOrWhiteSpace(config.Name)) {
+				release = $"{config.Name}@{version.Version}";
+			}
+		}
+
+		if (release is null) {
+			Logger.Debug("Skipping Sentry initialization because converter version could not be determined.");
+			return;
+		}
+
+		SentrySdk.Init(options => {
+			// A Sentry Data Source Name (DSN) is required.
+			// See https://docs.sentry.io/product/sentry-basics/dsn-explainer/
+			options.Dsn = config.SentryDsn;
+
+			// This option enables Sentry's "Release Health" feature.
+			options.AutoSessionTracking = false;
+
+			// This option is recommended for client applications only. It ensures all threads use the same global scope.
+			// If you're writing a background service of any kind, you should remove this.
+			options.IsGlobalModeEnabled = true;
+			
+			options.AttachStacktrace = false;
+
+			options.MaxBreadcrumbs = int.MaxValue;
+			options.MaxAttachmentSize = long.MaxValue;
+
+			options.Release = release;
+#if DEBUG
+			options.Environment = "Debug";
+#else
+			options.Environment = "Release"; 
+#endif
+		});
+		Logger.Debug("Sentry initialized.");
+	}
+
+	public void AddBreadcrumb(string text) => SentrySdk.AddBreadcrumb(text);
+
+	public void SendMessageToSentry(int processExitCode) {
 		LogLine? error = GetFirstErrorLogLineFromGrid();
 		if (error is not null) {
 			var sentryMessageLevel = error.Level == Level.Fatal ? SentryLevel.Fatal : SentryLevel.Error;
@@ -20,7 +76,7 @@ public static class SentryHelper {
 		}
 	}
 	
-	public static void SendMessageToSentry(string message, SentryLevel level) {
+	public void SendMessageToSentry(string message, SentryLevel level) {
 		// Identify user by IP address and machine/user name.
 		string name = Environment.MachineName;
 		if (string.IsNullOrWhiteSpace(name)) {
@@ -34,7 +90,8 @@ public static class SentryHelper {
 	}
 
 	private static LogLine? GetFirstErrorLogLineFromGrid() {
-		var gridAppender = LogManager.GetRepository().GetAppenders().First(a => a.Name == "grid");
+		var gridAppender = LogManager.GetRepository().GetAppenders()
+			.First(a => string.Equals(a.Name, "grid", StringComparison.OrdinalIgnoreCase));
 		if (gridAppender is LogGridAppender logGridAppender) {
 			return logGridAppender.LogLines
 				.FirstOrDefault(l => l.Level is not null && l.Level >= Level.Error);
