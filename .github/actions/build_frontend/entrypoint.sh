af6846f1ac45c399a3881b168304ec7ca4ef1fe0
IhateTrains <IhateTrains@users.noreply.github.com>
2024-09-14 19:02:34 +0100

    Add `self_contained` input (#602)

--- .github/actions/build_frontend/entrypoint.sh
+++ .github/actions/build_frontend/entrypoint.sh
@@ -9,15 +9,15 @@ then
   cd "${FRONTER_DIR}/Fronter.NET"
   if [ "$RUNNER_OS" = "Windows" ]
   then
-    dotnet publish -c Release -p:PublishProfile=win-x64 --output:"${GITHUB_WORKSPACE}/${RELEASE_DIR}"
+    dotnet publish -c Release -p:PublishProfile=win-x64 --output:"${GITHUB_WORKSPACE}/${RELEASE_DIR}" --self-contained $SELF_CONTAINED
   
   elif [ "$RUNNER_OS" = "Linux" ]
   then
-    dotnet publish -c Release -p:PublishProfile=linux-x64 --output:"${GITHUB_WORKSPACE}/${RELEASE_DIR}"
+    dotnet publish -c Release -p:PublishProfile=linux-x64 --output:"${GITHUB_WORKSPACE}/${RELEASE_DIR}" --self-contained $SELF_CONTAINED
  
   elif [ "$RUNNER_OS" = "macOS" ]
   then
-    dotnet publish -c Release -p:PublishProfile=osx-arm64 --output:"${GITHUB_WORKSPACE}/${RELEASE_DIR}"
+    dotnet publish -c Release -p:PublishProfile=osx-arm64 --output:"${GITHUB_WORKSPACE}/${RELEASE_DIR}" --self-contained $SELF_CONTAINED
     codesign --force -s - "${GITHUB_WORKSPACE}/${RELEASE_DIR}/ConverterFrontend"
     echo "Checking signature..."
     codesign -dv --verbose=4 "${GITHUB_WORKSPACE}/${RELEASE_DIR}/ConverterFrontend"
