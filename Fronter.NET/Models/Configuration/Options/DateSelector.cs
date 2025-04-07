1dbdd2cfb983435a5d921d722c9f4b4f2bcb8dbb
IhateTrains <IhateTrains@users.noreply.github.com>
2025-04-08 01:40:47 +0200

    Add text input mode to date selector (#836)
    * Add text input mode to date selector
    
    * Fix ClearValue not updating the displayed date in selector

--- Fronter.NET/Models/Configuration/Options/DateSelector.cs
+++ Fronter.NET/Models/Configuration/Options/DateSelector.cs
@@ -1,6 +1,8 @@
-﻿using commonItems;
+﻿using Avalonia.Data;
+using commonItems;
 using ReactiveUI;
 using System;
+using System.Linq;
 
 namespace Fronter.Models.Configuration.Options;
 
@@ -25,12 +27,10 @@ public sealed class DateSelector : ReactiveObject {
 	public bool Editable { get; private set; } = true; // editable unless disabled
 	public DateTimeOffset MinDate { get; set; } = DateTimeOffset.MinValue;
 	public DateTimeOffset MaxDate { get; set; } = DateTimeOffset.MaxValue;
-	private DateTimeOffset? dateTimeOffsetValue;
+
 	public DateTimeOffset? DateTimeOffsetValue {
-		get => dateTimeOffsetValue;
-		set {
-			this.RaiseAndSetIfChanged(ref dateTimeOffsetValue, value);
-		}
+		get;
+		set => this.RaiseAndSetIfChanged(ref field, value);
 	}
 
 	public Date? Value {
@@ -51,9 +51,65 @@ public sealed class DateSelector : ReactiveObject {
 		}
 	}
 
+	public string? TextValue {
+		get => Value?.ToString();
+		set {
+			if (string.IsNullOrWhiteSpace(value)) {
+				DateTimeOffsetValue = null;
+			} else {
+				var dateElements = value.Split('.').Where(x => !string.IsNullOrEmpty(x)).ToArray();
+				if (dateElements.Length >= 3) {
+					ValidateYearString(dateElements[0]);
+					ValidateMonthString(dateElements[1]);
+					ValidateDayString(dateElements[2]);
+				} else if (dateElements.Length == 2) {
+					ValidateYearString(dateElements[0]);
+					ValidateMonthString(dateElements[1]);
+				} else if (dateElements.Length == 1) {
+					ValidateYearString(dateElements[0]);
+				} else {
+					throw new DataValidationException($"'{value}' is not a valid date, it should be in the format YYYY.MM.DD.");
+				}
+
+				DateTimeOffsetValue = new Date(value).ToDateTimeOffset();
+			}
+			this.RaisePropertyChanged(nameof(TextValue));
+		}
+	}
+
+	private static void ValidateYearString(string value) {
+		if (!int.TryParse(value, out var year)) {
+			throw new DataValidationException($"'{value}' is not a valid integer.");
+		}
+	}
+	private static void ValidateMonthString(string value) {
+		if (!int.TryParse(value, out var month)) {
+			throw new DataValidationException($"'{value}' is not a valid integer.");
+		}
+		if (month is < 1 or > 12) {
+			throw new DataValidationException($"'{value}' is not a valid month, it should be between 1 and 12.");
+		}
+	}
+	private static void ValidateDayString(string value) {
+		if (!int.TryParse(value, out var day)) {
+			throw new DataValidationException($"'{value}' is not a valid integer.");
+		}
+		if (day is < 1 or > 31) {
+			throw new DataValidationException($"'{value}' is not a valid day, it should be between 1 and 31.");
+		}
+	}
+
 	public string? Tooltip { get; private set; }
 
+	public bool UseDatePicker {
+		get;
+		set => this.RaiseAndSetIfChanged(ref field, value);
+	} = false;
+
+	public void ToggleUseDatePicker() {
+		UseDatePicker = !UseDatePicker;
+	}
 	public void ClearValue() {
-		DateTimeOffsetValue = null;
+		TextValue = null;
 	}
 }
