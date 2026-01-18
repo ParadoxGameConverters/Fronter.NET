2182743db706d8810934a02d5c58b2970d25928b
iht <IhateTrains@users.noreply.github.com>
2026-01-18 01:08:49 +0100

    Use AppContext.BaseDirectory for reading configuration files

--- Fronter.NET/Models/Configuration/Config.cs
+++ Fronter.NET/Models/Configuration/Config.cs
@@ -38,7 +38,7 @@ internal sealed class Config {
 	public Config() {
 		var parser = new Parser();
 		RegisterKeys(parser);
-		var fronterConfigurationPath = Path.Combine("Configuration", "fronter-configuration.txt");
+		var fronterConfigurationPath = Path.Combine(AppContext.BaseDirectory, "Configuration", "fronter-configuration.txt");
 		if (File.Exists(fronterConfigurationPath)) {
 			parser.ParseFile(fronterConfigurationPath);
 			logger.Info("Frontend configuration loaded.");
@@ -46,7 +46,7 @@ internal sealed class Config {
 			logger.Warn($"{fronterConfigurationPath} not found!");
 		}
 
-		var fronterOptionsPath = Path.Combine("Configuration", "fronter-options.txt");
+		var fronterOptionsPath = Path.Combine(AppContext.BaseDirectory, "Configuration", "fronter-options.txt");
 		if (File.Exists(fronterOptionsPath)) {
 			parser.ParseFile(fronterOptionsPath);
 			logger.Info("Frontend options loaded.");
