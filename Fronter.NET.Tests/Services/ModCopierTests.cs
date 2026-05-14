4b5de36c56270445645d18f1224585248cb73268
IhateTrains <IhateTrains@users.noreply.github.com>
2026-05-14 20:27:36 +0200

    Bump dependencies (#982)
    * Bump dependencies
    
    * Use OrderedDictionary from standard library
    
    * Update ModCopierTests.cs

--- Fronter.NET.Tests/Services/ModCopierTests.cs
+++ Fronter.NET.Tests/Services/ModCopierTests.cs
@@ -1,7 +1,7 @@
 using Fronter.Models.Configuration;
 using Fronter.Services;
-using Open.Collections;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
