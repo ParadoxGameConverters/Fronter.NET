d01dc8baa45eeaacd605b791166b000239df4864
IhateTrains <IhateTrains@users.noreply.github.com>
2026-08-16 10:11:35 +0200

    Fix Meziantou warnings (#1000)
    * Fix Meziantou warnings
    
    * Update TranslationSource.cs

--- Fronter.NET/Extensions/TranslationSource.cs
+++ Fronter.NET/Extensions/TranslationSource.cs
@@ -265,8 +265,8 @@ internal sealed partial class TranslationSource : ReactiveObject {
 	}
 
 	public List<string> LoadedLanguages { get; } = [];
-	private readonly Dictionary<string, CultureInfo> languages = [];
-	private readonly Dictionary<string, Dictionary<string, string>> translations = []; // key, <language, text>
+	private readonly Dictionary<string, CultureInfo> languages = [with(StringComparer.OrdinalIgnoreCase)];
+	private readonly Dictionary<string, Dictionary<string, string>> translations = [with(StringComparer.Ordinal)]; // key, <language, text>
 
 	public string CurrentLanguage {
 		get;
